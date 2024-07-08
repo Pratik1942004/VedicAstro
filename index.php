c8a8dc8ffeb3711da382b3e0db59b82fd59fbf17
pratik_bansode <pratikbansode1904@gmail.com>
2024-07-08 20:43:58 +0530

    adding all files to vedic astro repository

---
+++ index.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VEDIC ASTRO & VASTU CONSUTANCY SERVICES</title>
+    <link rel="stylesheet" href="style.css">
+    <script>src="script.js"</script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Grandstander:wght@900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  </head>
+<body>
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color:red;
+  color: white;
+}
+
+.topnav .icon {
+  display: none;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a:not(:first-child) {display: none;}
+  .topnav a.icon {
+    float: right;
+    display: block;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .topnav.responsive {position: relative;}
+  .topnav.responsive .icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .topnav.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+}
+</style>
+</head>
+<body>
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#review">Review</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
+
+<style>
+body {margin:0;height:2000px;}
+
+.icon-bar {
+  position: fixed;
+  top: 60%;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+.icon-bar a {
+  display: block;
+  text-align: center;
+  padding: 16px;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 20px;
+}
+
+.icon-bar a:hover {
+  background-color: #000;
+}
+
+.facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.google {
+  background: #dd4b39;
+  color: white;
+}
+
+.linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+.youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.content {
+  margin-left: 75px;
+  font-size: 30px;
+}
+</style>
+<body>
+
+<div class="icon-bar">
+  <a href="https://www.facebook.com/Vedic-Astro-Consultancy-112523213922601/" class="facebook"><i class="fa fa-facebook"></i></a> 
+  <a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
+  <a href="#" class="google"><i class="fa fa-google"></i></a> 
+  <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+  <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> 
+</div>
+
+
+<div class="heading" style="padding-left:16px" >
+<center>
+<h1>VEDIC ASTRO & VASTU CONSULTANCY</h1>
+<center>
+ 
+  <h3>For better Health,Wealth,Relationship and Business.</h3>
+</div>
+
+
+<div class="top">
+<u><h2 style="color:brown;">Why Astrology is  important ?</h2></u>
+<img src="astro.jpg" alt="astrology" align="right" style="width:200px; height:200px; margin-right:5%;"/>
+  <p>Astrology is an ancient concept, as old as time, you can say.</p>
+  <p> It is an importance aspect of our lives – our past, present and</p>
+  <p> future. To a great extent, astrology is used to forecast and predict</p>
+  <p> future events and can also be used as a medium to get rid of any kind of</p>
+  <p> mishap related to planetary positions.</p><br>
+  <u><h2 style="color:brown;">Numerology  </h2></u>
+  <img src="num.jpg" alt="astrology" align="right" style="width:200px; height:200px; margin-right:5%;"/>
+<p>Just as astrology the planets and zodiac signs are connected to specific</p>
+<p> attributes within astrology, according to the teachings of numerology,</p>
+<p> certain numbers are associated with specific traits or themes. These </p>
+<p>numbers are used to offer insight on personality, future events, and even<p> 
+<p style="margin-bottom:4%;">life's greater purpose</p>
+</div>
+<hr>
+<div class="info">
+<h1>"How Astrology and Numerology affect  our daily life?"</h1>
+<br>
+<ul>
+  <li style="size:2%">
+<h2>How Astrology can solve our health issues? </h2>
+ </li>
+ <img src="ghealth.jpg" alt="health" align="left" /><hr>
+ <p>An astrologer can predict the types of disease that are likely to manifest, as well as the organs that are likely to be affected Your individual temperament shows which body systems are most likely to be affected when you fall ill, and therefore how a disease is likely to progress.</p><span></span>
+<hr>
+<li>
+<h2>Effect of Astrology in improving bad Relationships.</h2>
+</li>
+
+<img src="relation.jpg" alt="Lettuce" align="right" />
+<h4>When it comes to love and relationships, I often get the question, “Are we a good match?” Or, even better… “Is he or she THE ONE?”</h4>
+<p>For a truly in-depth understanding of a particular relationship, an astrologer would typically request the birthday, minute, year and location of the two people involved, and then would put together birth charts that plot all of the planetary alignments for each partner in the relationship and examine how they relate to one another.</p>
+</li>
+<hr>
+<li>
+  <h2>How Astrology affect your wealth ? </h2>
+</li>
+<img src="wealth.jpg" alt="wealth" align="left"/>
+<p style="margin-bottom:12%;">Astrology is highly related to the wealth and financial prospects of a person. Here are some of the basic factors that are responsible for money in your Kundli: The 2nd house in the horoscope indicates the sources of income, the inflow of funds, and the self-earned financial prosperity.</p>
+<hr>
+<li>
+  <h2>Astrology helps you to grow your Business.</h2>
+</li>
+<img src="business.jpg" alt="business" align="right"/>
+<p style="margin-left:2%; margin-bottom:11%;"> Selection of a business is a very important decision of life. There are many astrological factors which need to be considered. Astrological readings can also suggest whether you can enjoy smooth success in your business ventures. You must have heard of some people studying in one field with distinction, but later doing some other business. What is the reason behind this? We all know in astrology, it's help to grow your business.</p>
+<hr>
+<li>
+  <h2>Problem caused due to igonrance of Vastu</h2>
+</li>
+<img src="vastu1.jpg" alt="Lettuce" align="left" width="250" height="202"/>
+<p style="margin-bottom:1%;"> Often, people face various problems like financial crisis, ill health, emotional turbulence, etc. It is believed by vastu experts that these maladies are a result of the ignorance of vastu principles and their application in one's home.So we must consider Vastu for our home our business place.</p>
+<h4 >What if you have already purchased or paid the bookingn amount?</h4>
+<p style="margin-bottom:1%;">You don't need to panic you can use the fourth check of vastu ,<b> Space Programming.</b> through simple and effevtive Vastu techniques and remedies based on pure logic and proven science of modern vastu, you can balance the elements in a perticular gone.the use  of colors  , shapes,lights,metals and symbols are highly effective in this regard. </p>
+<h4 style="margin-bottom:3%;">You can use Maha Vastu reasoning and logics to help you attract more happiness,love and prosperty in your lives through your Vastu-perfect Homes or Apartments  </h4>
+<h4 style="margin-bottom:1%;"><b>Vastu Consideration Wile Selecting a New Home.</b></h4>
+<p style="margin-bottom:1%;"><b>If you are about to buy a new home or an apartment,keep a close check on the following Vastu aspects of the property.</b></p>
+<h4 style="margin-bottom:1%;"><b>Entrance</b></h4>
+<p style="margin-bottom:1%;">While evaluating the design plan or layout of a plot or a construction(flat or apartment),the first consideration should always be a good entrance.The entrance holds the key to ushering in positively and happiness for the antire family. </p>
+<p style="margin-bottom:1%;">Entrance are considerd the most important aspect of vastu.According to MahaVastu,each living space has 32 possible location that can function as an entrance of building.Each of these 32 location has their own significance and affects our lives accordingly. </p>
+<p style="margin-bottom:1%;">For instance,an entrance int he South-Wast zone,Vastu zone of cash ,result in delayed payments.Tje South-Westenterance is among the least Vastu-complaint entrace and families living in such home can face monetary and relationshp issue to great extent.</p>
+<p style="margin-bottom:1%;">ON the country,if you have an antrance in the North,you can excpect great success in monetary growth as well</p>
+<p style="margin-bottom:1%;">MahaVastu family firmly believes that if your chosen property or apartment does not have a Vastu-compliant entrace, you can stillbuy the property by applying certain simple and effective Vastu remedies.</p>
+</ul>
+
+<hr>
+</div><center>
+<hr style="background-color:black; width:200px; height:2px; margin-bottom:3%;">
+</center>
+<div id="contact"class="contact">
+<h2 style="text-align: center; text-size:20px;  font-family: 'Grandstander', cursive; margin-bottom:5%;">CONTACT US</h2>
+</div>
+<center>
+<img src="map.png" alt="Nature" class="Responsive" width="600" height="200">
+</center>
+
+<style>
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+<b><h3 style="margin-left:10%;">Contact:</h3></b>
+<a href="tel:9730539119" class="button" style="margin-left:10%;">Call Now</a>
+
+
+
+<b><h4 style="margin-left:10%;">K.Nagesh</h4></b>
+
+<p style="margin-left:10%;">(Astrologer and MahaVastu Consultant)</p>
+<a href="tel:7875112555" class="button" style="margin-left:10%; margin-top:2%;">Call Now</a>
+<h4 style="margin-left:10%;">Tel:9730539119</h4>
+<p style="margin-left:10%;">(Vedic Consultancy)</p>
+
+
+
+<b><h3 style="margin-left:10%; margin-top:5%;">Address:</h3></b>
+<a class="button" style="margin-left:10%; ">Get Address</a>
+<h4 style="margin-left:10%;">A/58 Karnik Nagar,</h4>
+<h4 style="margin-left:10%;">Solpaur 413006 </h4>
+<center><hr style="background-color:black; width:200px; height:2px; margin-bottom:3%;"></center>
+<h2 id="about" style="text-align: center; text-size:20px;  font-family: 'Grandstander', cursive; margin-bottom:5%;">ABOUT US</h2>
+<div class="container"><center>
+ 
+  <img src="logo.png" class="img-circle" alt="Cinque Terre" width="304" height="236"> 
+  <h4 style="margin:5%;"> We are Certified Astrologer of International Astrology Foundation. Experience staff , 1000+ satisfied customer 100% Guaranted solution. Best Astrologer , Vedic Astro Consultancy is the only solution  to the Business losses, Domestic disputes, and Broken Relationships. Many people live happy after adopting Vedic Astro Consultancy Services.    </h4>
+  </center>
+</div>
+<style>
+.footer {
+
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: white;
+   color: gray;
+   text-align: center;
+}
+</style>
+
+<center><hr style="background-color:black; width:200px; height:2px; margin-bottom:3%;"></center>
+
+<style>
+.contain {
+  border: 2px solid #ccc;
+  background-color: #eee;
+  border-radius: 5px;
+  padding: 16px;
+  margin: 16px 0
+}
+
+.contain::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.contain img {
+  float: left;
+  margin-right: 20px;
+  border-radius: 50%;
+}
+
+.contain span {
+  font-size: 20px;
+  margin-right: 15px;
+}
+
+@media (max-width: 500px) {
+  .contain {
+      text-align: center;
+  }
+  .contain img {
+      margin: auto;
+      float: none;
+      display: block;
+  }
+}
+</style>
+</head>
+<body>
+
+<center><h2 id="review">Review</h2></center>
+
+
+<div class="contain">
+ 
+  <p><span>Prathmesh Bansode.</span> 1 year ago.</p>
+  <p>It realy Works , after loss in my business I visited Vedic astro consultancy And i got a very good result.</p>
+</div>
+
+<div class="contain">
+  
+  <p><span>Aditya Joshi.</span> 11 months ago.</p>
+  <p>My relationship was not going good we had many fights we even reached to devorce but after visiting here our fight got's resolved </p>
+</div>
+
+<div class="contain">
+  
+  <p><span>Suhas Kukarni.</span> 6 months ago.</p>
+  <p> I built my house according to Vastu by Mr.Nagesh Kulkarni (vedic astro consultant) now i feel very pleasant and no problems. </p>
+</div>
+<hr>
+
+<div class="footer">
+  <p style="text-align:left;" >vedicvastuastro20@gmail.com
+  <span style="float:right;">	&#169 copyright 2020 All Rights Reserve
+
+  </p>
+</div>
+
+
+</body>
+</html>
